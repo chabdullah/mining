6c68cfca114ce698a56b04d23d38b060bb158a32
Adelchi97 <elena.faillace97@gmail.com>
2020-01-10 11:21:34 +0100

    add caption text to each element of "figures"

--- find_captions.py
+++ find_captions.py
@@ -78,26 +78,82 @@ def draw_captions():
     json_path = "./resources/json/modified_samples.json"
     with open(json_path) as json_file:
         data = json.load(json_file)
-        # scorre tutte le annotazioni
-        for f in data['figures']:
+    # scorre tutte le annotazioni
+    for f in data['figures']:
 
-            # cerca il pdf pagina a cui si riferisce
-            page_id = f['page_id']
-            for im in data['images']:
-                if page_id == im['id']:
-                    file_name = im['file_name']
-            file_path = jpeg_path + file_name
-            figure_bbox = f['figure_bbox']
-            caption_bbox = f['text_bbox']
-            # shape has the form ((x0, y0), (x1, y1))
-            figure_shape = ((figure_bbox[0], figure_bbox[1]), (figure_bbox[0] + figure_bbox[2]),
-                            (figure_bbox[1] + figure_bbox[3]))
-            caption_shape = ((caption_bbox[0], caption_bbox[1]), (caption_bbox[0] + caption_bbox[2]),
-                             (caption_bbox[1] + caption_bbox[3]))
+        # cerca il pdf pagina a cui si riferisce
+        page_id = f['page_id']
+        for im in data['images']:
+            if page_id == im['id']:
+                file_name = im['file_name']
+        file_path = jpeg_path + file_name
+        figure_bbox = f['figure_bbox']
+        caption_bbox = f['text_bbox']
+        # shape has the form ((x0, y0), (x1, y1))
+        figure_shape = ((figure_bbox[0], figure_bbox[1]), (figure_bbox[0] + figure_bbox[2]),
+                        (figure_bbox[1] + figure_bbox[3]))
+        caption_shape = ((caption_bbox[0], caption_bbox[1]), (caption_bbox[0] + caption_bbox[2]),
+                         (caption_bbox[1] + caption_bbox[3]))
 
-            # carica l'immagine jpeg
-            image = Image.open(file_path)
-            ImageDraw.Draw(image).rectangle(figure_shape, fill=None, outline="orange")
-            ImageDraw.Draw(image).rectangle(caption_shape, fill=None, outline="red")
-            image.save(file_path)
+        # carica l'immagine jpeg
+        image = Image.open(file_path)
+        ImageDraw.Draw(image).rectangle(figure_shape, fill=None, outline="orange")
+        ImageDraw.Draw(image).rectangle(caption_shape, fill=None, outline="red")
+        image.save(file_path)
+
+
+def find_caption_text():
+    path_jpeg = './resources/jpeg/'
+    path_xml = "./resources/xml/xml_optimazed/"
+    path_json = './resources/json/modified_samples.json'
+    with open(path_json) as json_file:
+        data = json.load(json_file)
+
+    for f in data['figures']:
+        caption_bbox = f['text_bbox']
+        page_id = f['page_id']
+        file_name = 'NULL'
+        caption_text = 'NULL'
+        # get xml file name
+        for p in data['images']:
+            if p['id'] == page_id:
+                file_name = p['file_name'].split('.')[0]
+
+        file_xml = open(path_xml + file_name + '.xml')
+        text_xml = file_xml.read()
+        bboxes = text_xml.split('bbox="')[2:]
+        # get page height
+        page_height = float(text_xml.split('<pages>')[1].split('bbox="')[1].split('"')[0].split(',')[3])
+        # find all bbox in the page
+        dist = 10000
+        for bbox in bboxes:
+            box = bbox.split('"')[0].split(',')
+            a = float(box[0])
+            b = page_height - float(box[3])
+            c = float(box[2]) - float(box[0])
+            d = float(box[3]) - float(box[1])
+            current_bbox = [a, b, c, d]
+            current_dist = \
+                           abs(current_bbox[0] - caption_bbox[0]) + \
+                           abs(current_bbox[0] + current_bbox[2] - caption_bbox[0] - caption_bbox[2]) + \
+                           abs(current_bbox[1] - caption_bbox[1]) + \
+                           abs(current_bbox[1] + current_bbox[3] - caption_bbox[1] - caption_bbox[3]) 
+
+            if current_dist < dist:
+                dist = current_dist
+                caption_text = bbox.split('>')[1].split('</textbox')[0]
+
+        # create new element in json file
+        f['text'] = caption_text
+        print('text found for figure on page ' + file_name)
+
+    with open(path_json, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+find_caption_text()
+
+
+
+
 
